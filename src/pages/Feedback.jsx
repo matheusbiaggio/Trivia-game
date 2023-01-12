7127b688df3594b20594699bb54048f0ba3cac5e
Matheus Biaggio <matheusbiaggio@gmail.br>
2023-01-12 15:02:42 -0300

    termino do requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,12 +1,32 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
+
+const NUMBER_THREE = 3;
 
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
     return (
-      <div><Header /></div>
+      <div>
+        <Header />
+        <p data-testid="feedback-text">
+          {
+            assertions < NUMBER_THREE ? 'Could be better...' : 'Well Done!'
+          }
+        </p>
+      </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = ({ player: { assertions } }) => ({
+  assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
