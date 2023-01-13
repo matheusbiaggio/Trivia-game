74d4c93e5a4a4606d1948d2322525bb924d4e888
cauan-oss <cauanbarros14@gmail.com>
2023-01-13 18:39:32 +0000

    requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,14 +1,33 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Ranking extends Component {
+  redirectHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          onClick={ this.redirectHome }
+          data-testid="btn-go-home"
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default connect()(Ranking);
